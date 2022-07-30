cf88638a4820ffbfa1a35c3050f07085830bbaac
Henrique Kleinberger <henriquedfkl@gmail.com>
2022-07-30 18:10:38 +0300

    navigation

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,15 +1,31 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import LogoImg from '../../public/logo.svg';
 import BasicLayout from '../../layout/BasicLayout';
-import { Container, Logo } from './styles';
+import { Container, Logo, Header, Navigation, Tab } from './styles';
+import { HOME, WISHLIST } from '../../constants/navigation';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+
   return (
     <BasicLayout>
       <Container>
-        <Logo>
-          <LogoImg alt="weiss logo" width="30" height="30" />
-          WEISS CINEMA
-        </Logo>
+        <Header>
+          <Logo>
+            <LogoImg alt="weiss logo" width="30" height="30" />
+            WEISS CINEMA
+          </Logo>
+          <Navigation>
+            <Link href={HOME}>
+              <Tab active={router.pathname === HOME}>home</Tab>
+            </Link>
+            <Link href={WISHLIST}>
+              <Tab active={router.pathname === WISHLIST}>wishlist</Tab>
+            </Link>
+          </Navigation>
+          <div />
+        </Header>
         {children}
         <footer></footer>
       </Container>
--- components/Layout/styles.ts
+++ components/Layout/styles.ts
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 
+interface ITab {
+  active: boolean;
+}
+
 export const Container = styled.div`
   padding: 0.5rem 2rem;
   min-height: 100vh;
   background-color: var(--background);
 `;
-export const Logo = styled.header`
+export const Logo = styled.div`
   color: var(--primary);
   fill: currentColor;
   align-items: center;
@@ -13,4 +17,23 @@ export const Logo = styled.header`
   font-weight: 500;
   letter-spacing: 1px;
   gap: 1rem;
+`;
+
+export const Header = styled.header`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+`;
+
+export const Navigation = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+export const Tab = styled.a<ITab>`
+  text-decoration: ${(props) => (props.active ? 'underline' : '')};
+  font-weight: ${(props) => (props.active ? '800' : '400')};
+  text-transform: uppercase;
+  color: var(--white);
+  cursor: pointer;
 `;
