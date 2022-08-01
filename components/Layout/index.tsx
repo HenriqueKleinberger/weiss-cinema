64aef40b6795328646f8468feca906286334cbf5
Henrique Kleinberger <henriquedfkl@gmail.com>
2022-08-01 20:30:09 +0300

    navigation removed

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,13 +1,8 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
 import LogoImg from '../../public/logo.svg';
 import BasicLayout from '../../layout/BasicLayout';
-import { Container, Logo, Header, Navigation, Tab } from './styles';
-import { HOME, WISHLIST } from '../../constants/navigation';
+import { Container, Logo, Header } from './styles';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const router = useRouter();
-
   return (
     <BasicLayout>
       <Container>
@@ -16,14 +11,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <LogoImg alt="weiss logo" width="30" height="30" />
             WEISS CINEMA
           </Logo>
-          <Navigation>
-            <Link href={HOME}>
-              <Tab active={router.pathname === HOME}>home</Tab>
-            </Link>
-            <Link href={WISHLIST}>
-              <Tab active={router.pathname === WISHLIST}>wishlist</Tab>
-            </Link>
-          </Navigation>
           <div />
         </Header>
         {children}
