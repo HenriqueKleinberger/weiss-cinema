43da82822cd40f1b9ab1af43fc90a508a2dc3c7a
Henrique Kleinberger <henriquedfkl@gmail.com>
2022-08-01 20:25:31 +0300

    bug fix

--- components/Wishlist/index.tsx
+++ components/Wishlist/index.tsx
@@ -1,11 +1,15 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Movie from '../../components/Movie';
 import { getMoviesFromLocalStorage } from '../../helpers/localStorageWishlist';
 import { IMovie } from '../../types';
 import EmptyWishlist from '../../components/FeedbackMessages/EmptyWishlist';
 
 const Wishlist = () => {
-  const [movies, setMovies] = useState<IMovie[]>(getMoviesFromLocalStorage());
+  const [movies, setMovies] = useState<IMovie[]>([]);
+
+  useEffect(() => {
+    setMovies(getMoviesFromLocalStorage());
+  }, []);
 
   const removeMovieFromWishlist = (movie: IMovie) =>
     setMovies(movies.filter((m) => m.imdbID !== movie.imdbID));
